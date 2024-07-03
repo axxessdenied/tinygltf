f934a30ac25564162fab65f6400e06f23a4bf0e9
Nick Slusarczyk <axxessdenied@gmail.com>
2024-07-02 20:22:42 -0400

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,16 +2,6 @@ sources = {
    "loader_example.cc",
    }
 
--- premake4.lua
-solution "TinyGLTFSolution"
-   configurations { "Release", "Debug" }
-
-   if (os.is("windows")) then
-      platforms { "x32", "x64" }
-   else
-      platforms { "native", "x32", "x64" }
-   end
-
    -- A project defines one build target
    project "tinygltf"
       kind "ConsoleApp"
@@ -22,9 +12,7 @@ solution "TinyGLTFSolution"
       configuration "Debug"
          defines { "DEBUG" } -- -DDEBUG
          flags { "Symbols" }
-         targetname "loader_example_tinygltf_debug"
-
+         
       configuration "Release"
          -- defines { "NDEBUG" } -- -NDEBUG
          flags { "Symbols", "Optimize" }
-         targetname "loader_example_tinygltf"
